4f1a1703237dd9c5c6ddf11678e32063d5bd7e3f
Abraham Perez <abraham Perez Quiroz>
2024-03-20 18:12:53 -0600

    a

--- db.py
+++ db.py
@@ -1,141 +1,105 @@
-from flask import Flask, render_template, request, redirect, session, make_response, url_for
-from functools import wraps
-from flask_login import LoginManager, UserMixin, login_required, login_user
-
-import pymongo
+from flask import Flask, render_template, request, redirect, session, url_for
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta_aqui'
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'iniciar_sesion'
+# Inicializar la aplicación de Firebase
+cred = credentials.Certificate('passqr-9141c-firebase-adminsdk-ahwbs-2c35df3e92.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-def get_connection():
-    return pymongo.MongoClient("mongodb://localhost:27017/")
+@app.route('/crear_usuarios', methods=['GET'])
+def crear_usuarios():
+    # Lista de usuarios con sus datos
+    usuarios_nuevos = [
+        {
+            'Matricula': '2022IDGS030',
+            'Nombre': 'Santiago',
+            'Apellidos': 'Montiel Sosa',
+            'Carrera': 'TIADSM',
+            'Usuario': 'SantiagoIDGS030',
+            'Contraseña': 'TIADSM030',
+            'Salida': '',
+            'Entrada': '',
+        },
+        {
+            'Matricula': '2022IDGS009',
+            'Nombre': 'Juan Manuel',
+            'Apellidos': 'Hernández Sánchez',
+            'Carrera': 'TIADSM',
+            'Usuario': 'JuanIDGS009',
+            'Contraseña': 'TIADSM009',
+            'Salida': '',
+            'Entrada': '',
+        },
+        {
+            'Matricula': '20221DGS059',
+            'Nombre': 'Andoni Agustín',
+            'Apellidos': 'López Sandoval',
+            'Carrera': 'TIADSM',
+            'Usuario': 'AndoniIDGS059',
+            'Contraseña': 'TIADSM059',
+            'Salida': '',
+            'Entrada': '',
+        },
+        {
+            'Matricula': '2022IDGS071',
+            'Nombre': 'Alvaro',
+            'Apellidos': 'Aguirre Palestina',
+            'Carrera': 'TIADSM',
+            'Usuario': 'AlvaroIDGS071',
+            'Contraseña': 'TIADSM071',
+            'Salida': '',
+            'Entrada': '',
+        },
+    ]
 
-class User(UserMixin):
-    pass
+    # Obtener una referencia a la colección 'usuarios' en Firestore
+    usuarios_ref = db.collection('usuarios')
 
-@login_manager.user_loader
-def load_user(user_id):
-    user = User()
-    user.id = user_id
-    return user
+    # Agregar cada usuario a la colección 'usuarios'
+    for usuario in usuarios_nuevos:
+        usuarios_ref.add(usuario)
 
-def Inicio_Requerido(view_func):
-    @wraps(view_func)
-    def decorated_function(*args, **kwargs):
-        if not session.get('Usuario'):
-            # Capturar la URL a la que intentaron acceder
-            session['next_url'] = request.url
-            return redirect(url_for('iniciar_sesion'))
-        return view_func(*args, **kwargs)
-    return decorated_function
+    return 'Usuarios creados correctamente'
 
 @app.route('/iniciar_sesion', methods=['GET', 'POST'])
 def iniciar_sesion():
-    mensaje = None
-
     if request.method == 'POST':
         usuario = request.form['usuario']
         contrasena = request.form['contrasena']
-        
-        # Crear una conexión a la base de datos
-        connection = get_connection()
 
-        # Seleccionar la colección de alumnos
-        mi_base_de_datos = connection["SecurePassQR"]
-        mi_coleccion = mi_base_de_datos["Alumnos"]
+        # Consultar Firebase para verificar el usuario y la contraseña
+        usuarios_ref = db.collection('usuarios')
+        query = usuarios_ref.where('Usuario', '==', usuario).where('Contraseña', '==', contrasena).limit(1)
+        results = query.get()
 
-        # Consultar la colección para verificar el usuario y la contraseña
-        usuario_encontrado = mi_coleccion.find_one({"Usuario": usuario, "Contraseña": contrasena})
-
-        connection.close()
-
-        if usuario_encontrado is None:
-            # Usuario no registrado o contraseña incorrecta uwu
+        if len(results) == 0:
+            # Usuario no registrado o contraseña incorrecta
             mensaje = 'Usuario no registrado o contraseña incorrecta'
-        else:
-            # Convertir ObjectId a cadena
-            usuario_encontrado['_id'] = str(usuario_encontrado['_id'])
+            return render_template('iniciar_sesion.html', mensaje=mensaje)
 
-            user = User()
-            user.id = usuario_encontrado['_id']
+        usuario_encontrado = results[0].to_dict()
 
-            login_user(user)
-            session['Usuario'] = usuario_encontrado
-
-            # Redirigir a la URL capturada o al visualizar si no hay URL capturada
-            next_url = session.pop('next_url', None)
-            response = make_response(redirect(next_url or '/visualizar'))
-            response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-            return response
-
-    return render_template('iniciar_sesion.html', mensaje=mensaje)
-
-@app.route('/visualizar')
-@Inicio_Requerido
-def visualizar():
-    usuario_actual = session['Usuario']
-
-    # Crear una conexión a la base de datos
-    connection = get_connection()
-
-    # Seleccionar la colección de alumnos
-    mi_base_de_datos = connection["SecurePassQR"]
-    mi_coleccion = mi_base_de_datos["Alumnos"]
-
-    # Consultar la colección para obtener los datos de los estudiantes
-    datos_estudiantes = list(mi_coleccion.find({}, {'_id': 0}))
-
-    # Convertir ObjectId a cadena en cada documento
-    for estudiante in datos_estudiantes:
-        estudiante['Matricula'] = str(estudiante['Matricula'])
-
-    connection.close()
-
-    return render_template('visualizacion.html', usuario=usuario_actual, datos_estudiantes=datos_estudiantes)
-
-@app.route('/registrar', methods=['GET', 'POST'])
-@Inicio_Requerido
-def registrar():
-    usuario_actual = session['Usuario']
-
-    if request.method == 'POST':
-        matricula = request.form['matricula']
-        nombre = request.form['nombre']
-        apellidos = request.form['apellidos']
-        carrera = request.form['carrera']
-        usuario = request.form['usuario']
-
-        contraseña = request.form['contraseña']
-
-        connection = get_connection()
-
-        mi_base_de_datos = connection["SecurePassQR"]
-        mi_coleccion = mi_base_de_datos["Alumnos"]
-
-        nuevo_estudiante = {
-            "Matricula": matricula,
-            "Nombre": nombre,
-            "Apellidos": apellidos,
-            "Carrera": carrera,
-            "Usuario": usuario,
-            "Contraseña": contraseña
-        }
-
-        mi_coleccion.insert_one(nuevo_estudiante)
-
-        connection.close()
-
+        session['user'] = usuario_encontrado
         return redirect('/visualizar')
 
-    return render_template('registrar.html', usuario=usuario_actual)
+    return render_template('iniciar_sesion.html')
 
-@app.route('/about')
-def about():
+@app.route('/visualizar')
+def visualizar():
+    if 'user' not in session:
+        return redirect(url_for('iniciar_sesion'))
 
-    return render_template('About.html')
+    usuario_actual = session['user']
+
+    # Consultar Firebase para obtener los datos de los estudiantes
+    alumnos_ref = db.collection('alumnos')
+    datos_estudiantes = [doc.to_dict() for doc in alumnos_ref.get()]
+
+    return render_template('visualizacion.html', usuario=usuario_actual, datos_estudiantes=datos_estudiantes)
 
 if __name__ == "__main__":
     app.run(debug=True)
